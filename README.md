5531cfb2503cb0f7bcbf4582169037b93d7c2359
Robbie Holmes <robbiethegeek@gmail.com>
2020-08-15 12:01:55 -0400

    Initial working code.

---
+++ README.md
@@ -0,0 +1,9 @@
+# What Is
+
+Environment variables: 
+    ACRONYMS_URL the url of the acronyms csv file [REQUIRED]
+    PORT to set the port that the app will run on (default: 3000) [OPTIONAL]
+
+Once running you can reqest at the url or IP address at the port specified and with a query parameter of acronym.
+
+http://localhost:3000/?acronym=stsi
---
+++ index.js
@@ -0,0 +1,46 @@
+const csv = require('csvtojson');
+const express = require("express");
+const { env, exit } = require('process');
+const fetch = require("node-fetch");
+const app = express();
+let dataSet;
+const search = input => dataSet.find(current => current.acronym == input);
+if (!env.ACRONYMS_URL) {
+    console.log("Please make sure there is a ACRONYMS_URL environment variable");
+    exit(1);
+}
+app.get("/", (req, res) => {
+    const sendResponse = (input) => {
+        const body = search(input);
+        if (body) {
+            res.send(body);
+        } else {
+            res.sendStatus(404)
+        }
+    }
+    const loadDataAndSendResponse = () => {
+        (async () => {
+            const response = await fetch(env.ACRONYMS_URL);
+            if (response.ok) {
+                const body = await response.text();
+                csv()
+                .fromString(body)
+                .then(jsonObj=>{
+                    dataSet=jsonObj;
+                    sendResponse(acronym);
+                });
+            }
+        })();
+    }
+    const acronym = (req.query.acronym) ? req.query.acronym.toString().toUpperCase() : undefined;
+    if (!acronym){
+        res.sendStatus(400);
+    } else {
+        if (!dataSet) {
+            loadDataAndSendResponse();
+        } else {
+            sendResponse(acronym)
+        }
+    }
+});
+app.listen((env.PORT) ? env.PORT : '80');
