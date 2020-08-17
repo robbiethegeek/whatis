4c92745b41a965328021b487913a4796fc20a8aa
Robbie Holmes <rholmes@stsiinc.com>
2020-08-17 15:28:46 -0400

    Add jest and tests for search.

---
+++ __tests__/search.test.js
@@ -0,0 +1,22 @@
+const { search } = require("../search");
+const testAcronym = "stsi";
+const expectedResponse = {
+  acronym: "STSI",
+  definition: "Solution Technology Systems Inc.",
+  context: "",
+  notes: "",
+};
+const testValid = [expectedResponse];
+
+test("verify that when empty data it returns undefined", () => {
+  expect(search(testAcronym, [])).toBe(undefined);
+});
+test("get back valid correct JSON object when acronym is found", () => {
+  expect(search(testAcronym, testValid)).toBe(expectedResponse);
+});
+test("verify that you get undefined when the acronym is not found.", () => {
+  expect(search("nope", testValid)).toBe(undefined);
+});
+test("verify that you get undefined when the acronym is empty.", () => {
+  expect(search("", testValid)).toBe(undefined);
+});
--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const express = require("express");
 const { env, exit } = require("process");
 const fetch = require("node-fetch");
 const app = express();
-let dataSet;
-const search = (input) => dataSet.find((current) => current.acronym == input);
+const { search } = require("./search");
+
 if (!env.ACRONYMS_URL) {
   console.log("Please make sure there is a ACRONYMS_URL environment variable");
   exit(1);
 }
 app.get("/", (req, res) => {
+  let dataSet;
   const sendResponse = (input) => {
-    const body = search(input);
+    const body = search(input, dataSet);
     if (body) {
       res.send(body);
     } else {
